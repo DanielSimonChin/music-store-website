2086f41d2e6651e028b1906c4bbe7624860776ac
victor <thevictors08@gmail.com>
2021-04-12 04:11:41 -0400

    completed admin banner ads

--- MusicalMoose/src/main/java/com/gb4w21/musicalmoose/controller/FileUploadController.java
+++ MusicalMoose/src/main/java/com/gb4w21/musicalmoose/controller/FileUploadController.java
@@ -67,26 +67,40 @@ public class FileUploadController implements Serializable {
      * @throws IOException
      */
     public void handleFileUpload(FileUploadEvent event) throws IOException {
+        fileUpload(event, "album_covers");
+    }
+    
+    /**
+     * Call the saveUploadedImage method to save the image to the project and
+     * display a success message to the user
+     *
+     * @param event
+     * @throws IOException
+     */
+    public void handleFileAdUpload(FileUploadEvent event) throws IOException {
+        fileUpload(event, "ads");
+    }
+    
+    private void fileUpload(FileUploadEvent event, String fileName) {
         this.file = null;
         UploadedFile file = event.getFile();
         if (file != null && file.getContent() != null && file.getContent().length > 0 && file.getFileName() != null) {
             this.file = file;
-            saveUploadedImage();
+            saveUploadedImage(fileName);
 
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Success", this.file.getFileName() + " is uploaded."));
             PrimeFaces.current().ajax().update("form:messages", "form:dt-products");
         }
-
     }
 
     /**
      * Saves the input image file to the directory containing all album covers.
      */
-    private void saveUploadedImage() {
+    private void saveUploadedImage(String fileName) {
         InputStream inputStream = null;
         try {
             String currentClassPath = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-            String destinationPath = currentClassPath.substring(0, currentClassPath.indexOf("target/")) + "src/main/webapp/album_covers/";
+            String destinationPath = currentClassPath.substring(0, currentClassPath.indexOf("target/")) + "src/main/webapp/" + fileName + "/";
             //String filename = FilenameUtils.getName(this.file.getFileName());
             inputStream = this.file.getInputStream();
             OutputStream outputStream = new FileOutputStream(new File(destinationPath, this.file.getFileName()));
@@ -106,11 +120,11 @@ public class FileUploadController implements Serializable {
      *
      * @return the list of filenames
      */
-    public List<String> getFileNames() {
+    public List<String> getFileNames(String fileName) {
         List<String> fileNames = new ArrayList<>();
 
         String currentPath = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-        String path = currentPath.substring(0, currentPath.indexOf("target/")) + "src/main/webapp/album_covers/";
+        String path = currentPath.substring(0, currentPath.indexOf("target/")) + "src/main/webapp/" + fileName + "/";
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
 
---
+++ MusicalMoose/src/main/java/com/gb4w21/musicalmoose/util/BannerAdManagerController.java
@@ -0,0 +1,176 @@
+package com.gb4w21.musicalmoose.util;
+
+import com.gb4w21.musicalmoose.controller.BanneradJpaController;
+import com.gb4w21.musicalmoose.controller.exceptions.NonexistentEntityException;
+import com.gb4w21.musicalmoose.entities.Bannerad;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.primefaces.PrimeFaces;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author victo
+ */
+@Named
+@SessionScoped
+public class BannerAdManagerController implements Serializable {
+
+    private final static Logger LOG = LoggerFactory.getLogger(BannerAdManagerController.class);
+    
+    @Inject
+    private BanneradJpaController bannerAdJpaController;
+    
+    private List<Bannerad> bannerAds;
+    private List<Bannerad> selectedBannerAds;
+    private Bannerad selectedBannerAd;
+    
+    
+    /**
+     * The data table should be filled with all the ad entity objects from
+     * the database.
+     */
+    @PostConstruct
+    public void init() {
+        this.bannerAds = bannerAdJpaController.findBanneradEntities();
+    }
+    
+    public List<Bannerad> getBannerAds() {
+        return this.bannerAds;
+    }
+    
+    public void setBannerAds(List<Bannerad> bannerAds) {
+        this.bannerAds = bannerAds;
+    }
+    
+    public List<Bannerad> getSelectedBannerAds() {
+        return this.selectedBannerAds;
+    }
+    
+    public void setSelectedBannerAds(List<Bannerad> selectedBannerAds) {
+        this.selectedBannerAds = selectedBannerAds;
+    }
+    
+    public Bannerad getSelectedBannerAd() {
+        return this.selectedBannerAd;
+    }
+    
+    public void setSelectedBannerAd(Bannerad selectedBannerAd) {
+        this.selectedBannerAd = selectedBannerAd;
+    }
+    
+    public void openNew() {
+        this.selectedBannerAd = new Bannerad();
+    }
+    
+    /**
+     * Check if the selected bannerAds is empty
+     *
+     * @return true if it is not empty
+     */
+    public boolean hasSelectedBannerAds() {
+        return this.selectedBannerAds != null && !this.selectedBannerAds.isEmpty();
+    }
+
+    /**
+     * Set the available table field for each selected bannerAd to false.
+     *
+     * @throws Exception
+     */
+    public void removeSelectedBannerAds() throws Exception {
+        for (Bannerad bannerAd : this.selectedBannerAds) {
+            bannerAd.setDisplayed(Boolean.FALSE);
+            this.bannerAdJpaController.edit(bannerAd);
+        }
+
+        this.selectedBannerAds = null;
+        FacesContext.getCurrentInstance().addMessage(null, com.gb4w21.musicalmoose.util.Messages.getMessage(
+                "com.gb4w21.musicalmoose.bundles.messages", "adSetNotDisplayed", null));
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-products");
+        PrimeFaces.current().executeScript("PF('dtProducts').clearFilters()");
+    }
+
+    /**
+     * Set the selected ad's available field to false.
+     *
+     * @throws Exception
+     */
+    public void removeBannerAd() throws Exception {
+        this.selectedBannerAd.setDisplayed(Boolean.FALSE);
+        this.bannerAdJpaController.edit(this.selectedBannerAd);
+        this.selectedBannerAd = null;
+        FacesContext.getCurrentInstance().addMessage(null, com.gb4w21.musicalmoose.util.Messages.getMessage(
+                "com.gb4w21.musicalmoose.bundles.messages", "adSetNotDisplayed", null));
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-products");
+    }
+
+    /**
+     * Update the selected ad and display a message for the user.
+     *
+     * @throws Exception
+     */
+    public void saveBannerAd() throws Exception {
+        if (checkValidAd()) {
+            // If this is a new track
+            if (this.selectedBannerAd.getBanneraddid() == null) {
+                LOG.info("CREATING A NEW BANNER AD ENTITY");
+                this.bannerAdJpaController.create(this.selectedBannerAd);
+                this.bannerAds.add(this.selectedBannerAd);
+                FacesContext.getCurrentInstance().addMessage(null, com.gb4w21.musicalmoose.util.Messages.getMessage(
+                        "com.gb4w21.musicalmoose.bundles.messages", "adCreated", null));
+                //A currently existing ad that was edited.
+            } else {
+                LOG.info("EDITING AN AD");
+                this.bannerAdJpaController.edit(this.selectedBannerAd);
+                FacesContext.getCurrentInstance().addMessage(null, com.gb4w21.musicalmoose.util.Messages.getMessage(
+                        "com.gb4w21.musicalmoose.bundles.messages", "adUpdated", null));
+            }
+        }
+        else {
+            FacesContext.getCurrentInstance().addMessage(null, com.gb4w21.musicalmoose.util.Messages.getMessage(
+                    "com.gb4w21.musicalmoose.bundles.messages", "adInvalid", null));
+        }
+        PrimeFaces.current().executeScript("PF('manageProductDialog').hide()");
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-products");
+    }
+    
+    /**
+     * Checks if the ad created/update is valid with the other ads
+     * 
+     * @return boolean
+     */
+    private boolean checkValidAd() {
+        if (selectedBannerAd.getDisplayed() && selectedBannerAd.getPageposition() > 0)
+        for (int i = 0; i < bannerAds.size(); i++) {
+            if (bannerAds.get(i).getDisplayed() && bannerAds.get(i).getPageposition() == selectedBannerAd.getPageposition()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * When the cancel button is clicked for the management form, all changes
+     * made before the cancel button was clicked will not affect the datatable
+     * or the database. Reset the datatable values.
+     *
+     * @throws NonexistentEntityException
+     */
+    public void cancelBannerAdForm() throws NonexistentEntityException {
+        this.bannerAds = this.bannerAdJpaController.findBanneradEntities();
+        PrimeFaces.current().executeScript("PF('manageProductDialog').hide()");
+        PrimeFaces.current().ajax().update("form:messages", "form:dt-products");
+    }
+}
